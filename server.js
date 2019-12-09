a170fdaef4f5d83f8364ecd453cab70af463e315
Evan Fredericksen <evfredericksen@gmail.com>
2019-12-08 21:30:55 -0500

    Make actual api request in /interventions

--- server.js
+++ server.js
@@ -1,78 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 3000;
 
 app.use('/static', express.static('dist'))
 
 app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
 
 app.get('/interventions', (req, res) => {
-    data = {
-        "terms": [
-            {
-                "name": "Antineoplastic Hormonal/Endocrine Agent",
-                "codes": [
-                    "C129818"
-                ],
-                "synonyms": [
-                    "Antineoplastic Endocrinal Agent",
-                    "Antineoplastic Hormonal Agent",
-                    "Antineoplastic Hormonal Therapeutic"
-                ],
-                "category": "agent category",
-                "count": 229
-            },
-            {
-                "name": "Apoptotic Pathway-targeting Antineoplastic Agent",
-                "codes": [
-                    "C129839"
-                ],
-                "synonyms": [
-                    "Apoptotic Pathway-targeting Cancer Therapeutic"
-                ],
-                "category": "agent category",
-                "count": 165
-            },
-            {
-                "name": "Cancer Diagnostic or Therapeutic Procedure",
-                "codes": [
-                    "C79426"
-                ],
-                "synonyms": [],
-                "category": "agent category",
-                "count": 9
-            },
-            {
-                "name": "Cancer Therapeutic Procedure",
-                "codes": [
-                    "C16212"
-                ],
-                "synonyms": [
-                    "Cancer Therapeutic Procedure",
-                    "Cancer Therapy",
-                    "Cancer Treatment",
-                    "Malignant Neoplasm Therapy",
-                    "Malignant Neoplasm Treatment"
-                ],
-                "category": "agent category",
-                "count": 9
-            },
-            {
-                "name": "Diagnostic, Therapeutic, or Research Equipment",
-                "codes": [
-                    "C19238"
-                ],
-                "synonyms": [
-                    "Diagnostic, Therapeutic, and Research Equipment",
-                    "Equipment",
-                    "Hardware"
-                ],
-                "category": "agent category",
-                "count": 34
-            }
-        ]
-    }
-    res.json({apiResults: data})
+    const searchTerm = encodeURIComponent(req.query.name)
+    request(`https://clinicaltrialsapi.cancer.gov/v1/interventions?size=25&name=${searchTerm}`, { json: true }, (err, apiResponse, body) => {
+        res.json({ apiResults: body })
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
