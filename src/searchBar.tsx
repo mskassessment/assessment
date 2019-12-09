d3d52be225c4cd1835dc26e5b718ef493b4fbcad
Evan Fredericksen <evfredericksen@gmail.com>
2019-12-08 22:49:14 -0500

    focus search field when page loads

--- src/searchBar.tsx
+++ src/searchBar.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextField, Button } from '@material-ui/core';
 import './styles.css';
 
 
 const SearchBar = (props: SearchBarProps) => {
 
+    const inputRef: any = React.useRef();
+    React.useEffect(() => {
+        if (inputRef !== undefined && inputRef.current !== undefined) {
+            inputRef.current.focus();
+        }
+    }, [])
+
     return (
         <div>
-            <TextField value={props.text} label="Search Interventions" onChange={evt => props.onChange(evt.target.value)} />
-            <Button variant="contained" color="primary"onClick={props.onSearch}>Search</Button>
+            <TextField inputRef={inputRef} value={props.text} label="Search Interventions" onChange={evt => props.onChange(evt.target.value)} />
+            <Button variant="contained" color="primary" onClick={props.onSearch}>Search</Button>
         </div>
     );
 }
